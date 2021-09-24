fd6bbeef7273f325165ed7d22ed943790b6210f7
Piotrullllo <prmatyja@gmail.com>
2021-09-24 20:10:39 +0200

    Day two

---
+++ projects/2-day-clock-app/js/main.js
@@ -0,0 +1,19 @@
+window.setInterval(timeCounter, 1);
+
+const secondHand = document.getElementById("Component_5_1");
+const minuteHand = document.getElementById("Component_6_1");
+const hourHand = document.getElementById("Component_7_1");
+const text = document.querySelector(".text");
+
+function timeCounter(){
+    let now = new Date;
+    let ms = now.getMilliseconds();
+    let sec = now.getSeconds();
+    let min = now.getMinutes();
+    let hour = now.getHours();
+    secondHand.style.transform = `rotate(${(sec*6)+(ms/166)}deg)`;
+    minuteHand.style.transform = `rotate(${(min*6)+(sec/10)}deg)`;
+    hourHand.style.transform = `rotate(${(hour*30)+(min/2)}deg)`;
+}
+
+

1f251af8dfb17c286e47f0b86770f273db7e0063
Piotrullllo <prmatyja@gmail.com>
2021-12-18 13:48:52 +0100

    Removed font from the cards, limited number of cards to 52, done some changes in scrolling physics

--- projects/27-click-and-drag-to-scroll/js/main.js
+++ projects/27-click-and-drag-to-scroll/js/main.js
@@ -10,16 +10,17 @@ function stopScrolling(){
     itemContainer.style.transform = "";
     itemContainer.style.cursor = "grab"
     interval = window.setInterval(() => {
-        if(offset < -2){
-            offset += 2;
-        } else if (offset > 2){
-            offset -= 2;
+        if(offset < -3){
+            offset += 3;
+        } else if (offset > 3){
+            offset -= 3;
         } else {
             offset = 0;
             window.clearInterval(interval);
         }
+        offset = Math.round(offset);
         itemContainer.scrollBy(offset,0);
-    }, 10)
+    }, 1000/60)
 }
 
 itemContainer.addEventListener("mousedown", event => {
@@ -36,7 +37,7 @@ itemContainer.addEventListener("mousedown", event => {
 itemContainer.addEventListener("mousemove", event => {
     if(toggleScroll && event.target.className != "item"){
         offset = (lastPosition-event.offsetX);
-        itemContainer.scrollBy(offset*2,0);
+        itemContainer.scrollBy(offset*1.5,0);
         lastPosition = event.offsetX;
     }
 });
@@ -45,7 +46,7 @@ itemContainer.addEventListener("mouseup", stopScrolling);
 window.addEventListener("mouseleave", stopScrolling);
 
 function itemsGenerator(){
-    for(i=0;i<99;i++){
+    for(i=0;i<52;i++){
         const item = document.createElement("div");
         item.setAttribute("class", "item");
         let r = Math.floor(Math.random()*(255 - 128)) + 128;
